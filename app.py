a66a78ec0faaa35b5a714bff5123b17db3d64107
tt yang <u253195@gmail.com>
2023-03-15 08:35:49 +0800

    global

--- app.py
+++ app.py
@@ -233,6 +233,6 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    uidlist=loaduid('userid.txt')
+#    uidlist=loaduid('userid.txt')
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
