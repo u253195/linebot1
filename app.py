74b6365bd51fcd835cea85626599cd3ab5ba9c49
tt yang <u253195@gmail.com>
2023-03-15 11:15:45 +0800

    global

--- app.py
+++ app.py
@@ -269,15 +269,15 @@ def handle_message(event):
             erfpid=event.message.text[4:len(event.message.text)]
             sss=erfValueStr(erfpid)
         elif event.message.text[:4]=="useradd@":
-            if src=uidlist[0]:
+            if src==uidlist[0]:
                 userinfo=event.message.text[8:len(event.message.text)]
                 adduser(userinfo)
         elif event.message.text[:4]=="userdel@":
-            if src=uidlist[0]:
+            if src==uidlist[0]:
                 userinfo=event.message.text[8:len(event.message.text)]
                 deluser(userinfo)
         elif event.message.text[:4]=="getusers":
-            if src=uidlist[0]:
+            if src==uidlist[0]:
                 sss=getusers()
         
         else:
